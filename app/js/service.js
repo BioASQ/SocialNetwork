12d3a59804119ec35fce4fd05d2d08f9234fee92
R. Speck <rene.speck@web.de>
2013-04-12 10:31:41 +0200

    add user service

--- app/js/service.js
+++ app/js/service.js
@@ -1,33 +1,84 @@
 'use strict';
-
-BioASQ.Questions = function ($rootScope, QuestionsRes, DetailRes) {
+/**
+ *  Questions service
+ */
+BioASQ.Questions = function ($rootScope, QuestionsRes, QuestionDetailRes) {
     this.scope = $rootScope;
     this.QuestionsRes = QuestionsRes;
-    this.DetailRes = DetailRes;
+    this.QuestionDetailRes = QuestionDetailRes;
 };
 
 
 BioASQ.Questions.prototype.getQuestions = function (callback) {
     //var self = this;
     this.QuestionsRes.get(
-        function (data, headers) { // on success
-            // self.scope.questions = data;
+        function (data, headers) {
             callback(data);
         },
-        function (response) { // on error
-            //self.scope.questions = "error";
+        function (response) {
             callback(null);
         });
 };
 
 BioASQ.Questions.prototype.getDetail = function (id, callback) {
-    this.DetailRes.get({ id: id },
-        function (data, headers) { // on success
+    this.QuestionDetailRes.get({ id: id },
+        function (data, headers) {
             callback(data[0]);
         },
-        function (response) { // on error
+        function (response) {
+            callback(null);
+        });
+};
+BioASQ.service('Questions', BioASQ.Questions);
+
+/**
+ * Users service
+ */
+BioASQ.Users = function ($rootScope, UserRes,CommentsRes,FollowersRes,FollowingRes) {
+    this.scope = $rootScope;
+    this.UserRes = UserRes;
+    this.CommentsRes = CommentsRes;
+    this.FollowersRes = FollowersRes;
+    this.FollowingRes = FollowingRes;
+};
+
+BioASQ.Users.prototype.getUser = function (id, callback) {
+    this.UserRes.get({ id: id },
+        function (data, headers) {
+            callback(data[0]);
+        },
+        function (response) {
             callback(null);
         });
 };
 
-BioASQ.service('BioAsqService', BioASQ.Questions);
+BioASQ.Users.prototype.getComments = function (id, callback) {
+    this.CommentsRes.get({ id: id },
+        function (data, headers) {
+            callback(data);
+        },
+        function (response) {
+            callback(null);
+        });
+};
+
+BioASQ.Users.prototype.getFollowers = function (id, callback) {
+    this.FollowersRes.get({ id: id },
+        function (data, headers) {
+            callback(data);
+        },
+        function (response) {
+            callback(null);
+        });
+};
+
+BioASQ.Users.prototype.getFollowing = function (id, callback) {
+    this.FollowingRes.get({ id: id },
+        function (data, headers) {
+            callback(data);
+        },
+        function (response) {
+            callback(null);
+        });
+};
+BioASQ.service('Users', BioASQ.Users);
