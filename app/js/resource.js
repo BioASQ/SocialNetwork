06ebaecc6e3c15cf566b8279341f577e97ef46c5
R. Speck <rene.speck@web.de>
2013-04-12 10:31:28 +0200

    add user resource

--- app/js/resource.js
+++ app/js/resource.js
@@ -1,20 +1,48 @@
 'use strict';
-
 BioASQ.factory('QuestionsRes', function ($resource, $window) {
     return $resource(
-        'http://' + window.location.host + ':' + window.location.port + '/data/questions.js', 
+        'http://' + $window.location.host + ':' + $window.location.port + '/data/questions.js',
         { callback: 'JSON_CALLBACK' },
         { get: { method: 'GET', isArray: true } }
     );
 });
 
-BioASQ.factory('DetailRes', function ($resource, $window) {
+BioASQ.factory('QuestionDetailRes', function ($resource, $window) {
     return $resource(
-        'http://' + window.location.host + ':' + window.location.port + '/data/detail/:id/detail.js', 
+        'http://' + $window.location.host + ':' + $window.location.port + '/data/detail/:id/detail.js',
         { id: "@id", callback: 'JSON_CALLBACK' },
         { get: { method: 'GET', isArray: true } }
     );
 });
 
-// how to use $window ? 
-// use instead of window
+BioASQ.factory('UserRes', function ($resource, $window) {
+    return $resource(
+        'http://' + $window.location.host + ':' + $window.location.port + '/data/user/:id/user.js',
+        { id: "@id", callback: 'JSON_CALLBACK' },
+        { get: { method: 'GET', isArray: true } }
+    );
+});
+
+BioASQ.factory('CommentsRes', function ($resource, $window) {
+    return $resource(
+        'http://' + $window.location.host + ':' + $window.location.port + '/data/user/:id/comments.js',
+        { id: "@id", callback: 'JSON_CALLBACK' },
+        { get: { method: 'GET', isArray: true } }
+    );
+});
+
+BioASQ.factory('FollowersRes', function ($resource, $window) {
+    return $resource(
+        'http://' + $window.location.host + ':' + $window.location.port + '/data/user/:id/followers.js',
+        { id: "@id", callback: 'JSON_CALLBACK' },
+        { get: { method: 'GET', isArray: true } }
+    );
+});
+
+BioASQ.factory('FollowingRes', function ($resource, $window) {
+    return $resource(
+        'http://' + $window.location.host + ':' + $window.location.port + '/data/user/:id/following.js',
+        { id: "@id", callback: 'JSON_CALLBACK' },
+        { get: { method: 'GET', isArray: true } }
+    );
+});
