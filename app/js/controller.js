7c42b587fdf56ff771e379c9aede088018990c3e
R. Speck <rene.speck@web.de>
2013-04-12 10:31:07 +0200

    add user controller

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,47 +1,81 @@
 'use strict';
-
-BioASQ.controller('HomeCtrl', function ($scope, BioAsqService) {
-
+/**
+ *
+ */
+BioASQ.HomeCtrl = function ($scope, $location, Questions) {
+    $scope.path = $location.path();
     // get questions
-    BioAsqService.getQuestions(function (data) {
+    Questions.getQuestions(function (data) {
         $scope.questions = data != null ? data : "error";
     });
 
-
     // init detail
-    $scope.detail    = {};
-    $scope.detail.id = "";
+    $scope.detail = { id : "" };
 
-    // ng-click
+    // ng-click detail
     $scope.questionDetail = function (id){
         // hide
         if ($scope.detail.id == id) {
             $scope.detail.id = "";
         } else {
             // show
-            BioAsqService.getDetail(id, function (data) {
-                if (data) {
-                    $scope.detail.id     = id;
-                    $scope.detail.answer = data.answer;
-                    $scope.detail.ideal  = data.ideal;
+            Questions.getDetail(id, function (data) {
+                if (data != null) {
+                    $scope.detail = {
+                                        id : id,
+                                        answer : data.answer,
+                                        ideal : data.ideal
+                                    }
                 }
             });
         }
     };
+};
 
-});
+/**
+ *
+ */
+BioASQ.UserCtrl =  function ($scope, $location, Users) {
+    // http://.../#/user/id
+    var id = $location.path().substr($location.path().lastIndexOf('/') + 1);
+    Users.getUser(id, function (data) {
+        $scope.user = data != null ? data : "error";
+    });
+    $scope.showComments = function(id){
+        Users.getComments(id, function(data){
+            $scope.data = data;
+            $scope.state = "comments";
+        });
+    };
+    $scope.showFollowers = function(id){
+        Users.getFollowers(id, function(data){
+            $scope.data = data;
+            $scope.state = "followers";
+        });
+    };
+    $scope.showFollowing = function(id){
+        Users.getFollowing(id, function(data){
+            $scope.data = data;
+            $scope.state = "following";
+        });
+    };
+};
 
+/**
+ *
+ */
+BioASQ.TimelineCtrl = function ($scope, $location) {
+    $scope.path = $location.path();
+};
 
-BioASQ.controller('TimelineCtrl', function ($scope, $location) {
-    $scope.title = $location.path();
-});
+/**
+ *
+ */
+BioASQ.MessageCtrl =  function ($scope, $location) {
+    $scope.path = $location.path();
+};
 
-
-BioASQ.controller('CommentsCtrl', function ($scope, $location) {
-    $scope.title = $location.path();
-});
-
-
-BioASQ.controller('UserCtrl', function ($scope, $location) {
-    $scope.title = $location.path();
-});
+BioASQ.controller('HomeCtrl', BioASQ.HomeCtrl);
+BioASQ.controller('UserCtrl', BioASQ.UserCtrl);
+BioASQ.controller('TimelineCtrl', BioASQ.TimelineCtrl);
+BioASQ.controller('MessageCtrl', BioASQ.MessageCtrl);
