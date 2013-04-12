aa7558bce4dff313b9a5a508da731a08f44432a8
R. Speck <rene.speck@web.de>
2013-04-12 10:30:46 +0200

    fix coding style

--- app/js/app.js
+++ app/js/app.js
@@ -3,9 +3,28 @@
 var BioASQ = angular.module('BioASQ', ['ngResource']);
 
 BioASQ.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
-    $routeProvider.when('/timeline', {templateUrl: 'partials/timeline.html', controller: 'TimelineCtrl'});  
-    $routeProvider.when('/comments', {templateUrl: 'partials/comments.html', controller: 'CommentsCtrl'});
-    $routeProvider.when('/user/:creator', {templateUrl: 'partials/user.html', controller: 'UserCtrl'});
-    $routeProvider.otherwise({redirectTo: '/home'});
+
+    $routeProvider.when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'HomeCtrl'
+    });
+
+    $routeProvider.when('/timeline', {
+        templateUrl: 'partials/timeline.html',
+        controller: 'TimelineCtrl'
+    });
+
+    $routeProvider.when('/message', {
+        templateUrl: 'partials/messages.html',
+        controller: 'MessageCtrl'
+    });
+
+    $routeProvider.when('/user/:creator', {
+        templateUrl: 'partials/user.html',
+        controller: 'UserCtrl'
+    });
+
+    $routeProvider.otherwise({
+        redirectTo: '/home'
+    });
 }]);
