067e27b02637310be559e3eb01a4340f1b404dd0
R. Speck <rene.speck@web.de>
2013-04-15 13:33:08 +0200

    add login path, update vote

--- bin/server.py
+++ bin/server.py
@@ -15,66 +15,66 @@ questions['1'] = json.loads(
 '{"_id" : "1", "body": "Are CNEs particularly enriched in gene deserts?", "type": "yes/no", "creatorID": ' + users['1']['_id'] + ', "creator" : "' + users['1']['name'] + '", "date": "2013-02-26", "comments": [1,2] , "rank": 42 , "answer" : "yes", "ideal" : "more details"}'
 )
 questions['2'] = json.loads(
-'{"_id" : "2", "body": "Are CNEs particularly enriched in gene deserts?", "type": "yes/no", "creatorID": ' + users['2']['_id'] + ', "creator" : "' + users['2']['name'] + '", "date": "2013-02-27", "comments": [3,4,5] , "rank": 13, "answer" : "no", "ideal" : "more details"}'
+'{"_id" : "2", "body": "Are CNEs particularly enriched in gene deserts?", "type": "yes/no", "creatorID": ' + users['2']['_id'] + ', "creator" : "' + users['2']['name'] + '", "date": "2013-02-27", "comments": [3,4,5] , "rank": 1337, "answer" : "no", "ideal" : "more details"}'
 )
 #################################
 comments = {}
 comments['1'] = json.loads(
-'{"_id" : "1", "creator": '+json.dumps(users['2']) +', "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere ipsum eget mauris ultricies consequat. Maecenas rhoncus commodo venenatis."}'
+'{"_id" : "1", "creator": ' + json.dumps(users['2']) + ', "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere ipsum eget mauris ultricies consequat. Maecenas rhoncus commodo venenatis."}'
 )
 comments['2'] = json.loads(
-'{"_id" : "2", "creator": '+json.dumps(users['1']) +', "comment": "Lorem ipsum dolor sit amet..."}'
+'{"_id" : "2", "creator": ' + json.dumps(users['1']) + ', "comment": "Lorem ipsum dolor sit amet..."}'
 )
 
 #################################
 following = {}
 following['1'] = json.loads(
-'['+
-json.dumps(users['2']) +','+
+'[' +
+json.dumps(users['2']) + ',' +
 json.dumps(questions['2']) +
 ']')
 
 following['2'] = json.loads(
-'['+
-json.dumps(users['1']) +','+
-json.dumps(questions['1']) +','+ 
+'[' +
+json.dumps(users['1']) + ',' +
+json.dumps(questions['1']) + ',' +
 json.dumps(questions['2']) +
 ']')
 #################################
 followers = {}
 followers['1'] = json.loads(
-'['+
+'[' +
 json.dumps(users['2']) +
 ']')
 
 followers['2'] = json.loads(
-'['+
+'[' +
 json.dumps(users['1']) +
 ']')
 #################################
 userComments = {}
 userComments['1'] = json.loads(
-'['+
+'[' +
 json.dumps(comments['1']) +
 ']')
 
 userComments['2'] = json.loads(
-'['+
+'[' +
 json.dumps(comments['2']) +
 ']')
 #################################
 @get('/following/:id')
-def userFollowing(id):
+def userFollowingRes(id):
     global following
     return json.dumps(following[id])
 
 @get('/followers/:id')
-def userFollowers(id):
+def userFollowersRes(id):
     global followers
     return json.dumps(followers[id])
 
 @get('/comments/:id')
-def userFollowers(id):
+def userCommentsRes(id):
     global userComments
     return json.dumps(userComments[id])
 
@@ -93,25 +93,28 @@ def questionsRes():
     global questions
     return json.dumps(questions)
 
-data = 0
 @post('/vote/:id')
 def voteRes(id):
-    global data 
     global questions
 
-    dir = str(request.forms.get('dir'))
-    
+    #dir = str(request.forms.get('dir'))
+    dir = str(request.query.get('dir'))
+
     if dir == 'up':
         questions[id]['rank'] =  int(questions[id]['rank']) + 1
     if dir == 'down':
         questions[id]['rank'] =  int(questions[id]['rank']) - 1
 
-    return str(data)
+    return str(questions[id]['rank'])
+
+@get('/login')
+def loginRes():
+    global users
+    return '[' + json.dumps(users['1']) + ']'
    
 @route('/:filename#.*#')
 def send_static(filename):
     return static_file(filename, root='./')
-
 
 bottle.debug(True) 
 run(host='localhost', port=8000)
