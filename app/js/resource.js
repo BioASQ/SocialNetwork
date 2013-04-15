58969f8e58ad197bf9d794244315d510ce62631c
R. Speck <rene.speck@web.de>
2013-04-15 13:35:40 +0200

    add login and vote

--- app/js/resource.js
+++ app/js/resource.js
@@ -1,7 +1,6 @@
 'use strict';
-/**
- *
- **/
+
+// /questions
 BioASQ.factory('QuestionsRes', function ($resource, $window) {
     return $resource(
         'http://' + $window.location.host + ':' + $window.location.port + '/questions',
@@ -10,44 +9,65 @@ BioASQ.factory('QuestionsRes', function ($resource, $window) {
     );
 });
 
+// /questions/:id
 BioASQ.factory('QuestionDetailRes', function ($resource, $window) {
     return $resource(
         'http://' + $window.location.host + ':' + $window.location.port + '/questions/:id',
-        { id: "@id", callback: 'JSON_CALLBACK' },
+        { id: '@id', callback: 'JSON_CALLBACK' },
         { get: { method: 'GET', isArray: true } }
     );
 });
-/**
- *
- **/
+
+// /users/:id
 BioASQ.factory('UserRes', function ($resource, $window) {
     return $resource(
         'http://' + $window.location.host + ':' + $window.location.port + '/users/:id',
-        { id: "@id", callback: 'JSON_CALLBACK' },
+        { id: '@id', callback: 'JSON_CALLBACK' },
         { get: { method: 'GET', isArray: true } }
     );
 });
 
+// /comments/:id
 BioASQ.factory('CommentsRes', function ($resource, $window) {
     return $resource(
         'http://' + $window.location.host + ':' + $window.location.port + '/comments/:id',
-        { id: "@id", callback: 'JSON_CALLBACK' },
+        { id: '@id', callback: 'JSON_CALLBACK' },
         { get: { method: 'GET', isArray: true } }
     );
 });
 
+// /followers/:id
 BioASQ.factory('FollowersRes', function ($resource, $window) {
     return $resource(
         'http://' + $window.location.host + ':' + $window.location.port + '/followers/:id',
-        { id: "@id", callback: 'JSON_CALLBACK' },
+        { id: '@id', callback: 'JSON_CALLBACK' },
         { get: { method: 'GET', isArray: true } }
     );
 });
 
+// /following/:id
 BioASQ.factory('FollowingRes', function ($resource, $window) {
     return $resource(
         'http://' + $window.location.host + ':' + $window.location.port + '/following/:id',
-        { id: "@id", callback: 'JSON_CALLBACK' },
+        { id: '@id', callback: 'JSON_CALLBACK' },
         { get: { method: 'GET', isArray: true } }
+    );
+});
+
+// /vote/:id
+BioASQ.factory('VoteRes', function ($resource, $window) {
+    return $resource(
+        'http://' + $window.location.host + ':' + $window.location.port + '/vote/:id',
+        { id: '@id', callback: 'JSON_CALLBACK' },
+        { post: { method: 'POST', params:{ dir : '@dir'}, isArray: true } }
+    );
+});
+
+// /login
+BioASQ.factory('MeRes', function ($resource, $window) {
+    return $resource(
+        'http://' + $window.location.host + ':' + $window.location.port + '/login',
+        { callback: 'JSON_CALLBACK' },
+        { login: { method: 'GET', isArray: true } }
     );
 });
