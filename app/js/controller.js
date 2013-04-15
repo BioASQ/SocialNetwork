ceedda16397a96024314b352210068a645706dfb
R. Speck <rene.speck@web.de>
2013-04-15 13:39:58 +0200

    update all ctrl with a new service

--- app/js/controller.js
+++ app/js/controller.js
@@ -2,7 +2,12 @@
 /**
  *
  */
-BioASQ.HomeCtrl = function ($scope, $location, Questions) {
+BioASQ.HomeCtrl = function ($scope, $location, Questions, Me) {
+
+    Me.login(function(){
+        $scope.me = Me.data;
+    });
+
     // get questions
     Questions.getQuestions(function (data) {
         $scope.questions = data != null ? data : "error";
@@ -13,28 +18,26 @@ BioASQ.HomeCtrl = function ($scope, $location, Questions) {
 
     // ng-click detail
     $scope.questionDetail = function (id){
-        // hide
-        if ($scope.detail.id == id) {
-            $scope.detail.id = "";
-        } else {
-            // show
-            Questions.getDetail(id, function (data) {
-                if (data != null) {
-                    $scope.detail = {
-                                        id : id,
-                                        answer : data.answer,
-                                        ideal : data.ideal
-                                    }
-                }
-            });
-        }
+        // show
+        Questions.getDetail(id, function (data) {
+            if (data != null) {
+                $scope.detail = {
+                                    id : id,
+                                    answer : data.answer,
+                                    ideal : data.ideal
+                                }
+            }
+        });
     };
 };
 
 /**
  *
  */
-BioASQ.UserCtrl =  function ($scope, $location, Users) {
+BioASQ.UserCtrl = function ($scope, $location, Users, Me) {
+    Me.login(function(){
+        $scope.me = Me.data;
+    });
     // http://.../#/user/id
     var id = $location.path().substr($location.path().lastIndexOf('/') + 1);
     Users.getUser(id, function (data) {
@@ -65,13 +68,19 @@ BioASQ.UserCtrl =  function ($scope, $location, Users) {
 /**
  *
  */
-BioASQ.TimelineCtrl = function ($scope, $location) {
+BioASQ.TimelineCtrl = function ($scope, $location, Me) {
+    Me.login(function(){
+        $scope.me = Me.data;
+    });
 };
 
 /**
  *
  */
-BioASQ.MessageCtrl =  function ($scope, $location) {
+BioASQ.MessageCtrl = function ($scope, $location, Me) {
+    Me.login(function(){
+        $scope.me = Me.data;
+    });
 };
 
 BioASQ.controller('HomeCtrl', BioASQ.HomeCtrl);
