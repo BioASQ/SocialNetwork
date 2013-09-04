a34c6a7f3aa66b45c56650db814e9440ca0d9f77
renespeck <speckrene@gmail.com>
2013-09-04 11:36:40 +0200

    Add redirect to login.

--- app/js/controller_registration.js
+++ app/js/controller_registration.js
@@ -1,11 +1,19 @@
 'use strict';
 
-BioASQ.RegistrationCtrl = function($routeParams, $scope) {
+BioASQ.RegistrationCtrl = function($routeParams, $scope, $location) {
     $scope.currentCtrl = 'RegistrationCtrl';
 
     $scope.registration = {
         code : $routeParams['code'] ? $routeParams['code'] : ''
     }
+
+    $scope.registration.cancle = function(){
+        $location.path( "login" );
+    };
+
+    $scope.registration.submit = function(){
+        //TODO
+    };
 };
 
 BioASQ.controller('RegistrationCtrl', BioASQ.RegistrationCtrl);
