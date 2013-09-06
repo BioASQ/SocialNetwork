d662bb17414ead5f1afcab56b6d694b3374af1d2
renespeck <speckrene@gmail.com>
2013-09-06 05:43:29 +0200

    Add login and activities.

--- app/js/controller_login.js
+++ app/js/controller_login.js
@@ -2,12 +2,31 @@
 
 var controllers = angular.module('bioasq.controllers');
 
-controllers.controller('LoginCtrl', function($routeParams, $scope, $location) {
+controllers.controller('LoginCtrl', function($rootScope, $scope, $location, MeRes, Activity) {
     $scope.currentCtrl = 'LoginCtrl';
 
-    $scope.login = {};
-    $scope.login.submit = function(){
-        //TODO
-        $location.path( "home" );
+    $scope.login = {
+        email: '',
+        password: '',
+        error: ''
     };
+
+    $scope.login.submit = function(){
+        MeRes.login({id: $scope.login.email, password: $scope.login.password},
+            function (user, headers) {
+                if(user.id !== 'anonymous'){
+                    $rootScope.me = user;
+                    Activity.following({ id: user.id }, function (result) {
+                        $rootScope.cache.followings = result.map(function (f) {
+                            return f.about;
+                        });
+                    });
+                    $location.path( "home" );
+                }
+            },
+            function(response) {
+                $scope.login.error = response;
+            }
+        );
+    }
 });
