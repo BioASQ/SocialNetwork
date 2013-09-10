ed5b5d495784d28e908812d39e1f46b56f11d849
Norman Heino <norman.heino@gmail.com>
2013-09-10 15:51:11 +0200

    Bump angular-ui to 0.6.
    Use $modal instead of $dialog.

--- app/js/controller_home.js
+++ app/js/controller_home.js
@@ -2,8 +2,7 @@
 
 var controllers = angular.module('bioasq.controllers');
 
-controllers.controller('HomeCtrl', function($scope, Activity, modalFactory) {
-    $scope.currentCtrl = 'HomeCtrl';
+controllers.controller('HomeCtrl', function($scope, Activity) {
     $scope.$watch('me.id', function () {
         $scope.activities = Activity.home({ id: $scope.me.id }, function () {
             $scope.activitiesFetched = true;
