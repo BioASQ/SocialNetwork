d3e82c8430caa84d70adfaa5fa5ccbac5842d924
Norman Heino <norman.heino@gmail.com>
2013-12-02 13:48:39 +0100

    Add db v1 migration script

---
+++ server/scripts/migration.js
@@ -0,0 +1,60 @@
+function makeID(tempID) {
+    if (typeof tempID !== 'string') {
+        return tempID;
+    }
+    if (tempID.length === 24) {
+        return new ObjectId(tempID);
+    }
+    var numberID = parseInt(tempID, 10);
+    if (numberID.toString(10) === tempID) {
+        return numberID;
+    }
+    return tempID;
+}
+
+function convertToIDs(object, properties) {
+    var self = this;
+    properties.forEach(function (propertyName) {
+        if (object.hasOwnProperty(propertyName)) {
+            object[propertyName] = self.makeID(object[propertyName]);
+        }
+    });
+    return object;
+}
+
+db.activity.find().forEach(function (a) {
+    a = convertToIDs(a, ['creator', 'about', 'reply_of']);
+    if (a.type === 'Question') a.type = 'Import';
+    db.activity.save(a);
+});
+
+db.message.find().forEach(function (m) {
+    db.message.save(convertToIDs(m, ['creator', 'to', 'reply_of']));
+});
+
+db.question.find().forEach(function (q) {
+    if (q.answer && q.answer.annotations) {
+        q.concepts   = q.answer.annotations.filter(function (a) { return (a.type === 'concept'); });
+        q.documents  = q.answer.annotations.filter(function (a) { return (a.type === 'document'); });
+        q.snippets   = q.answer.annotations.filter(function (a) { return (a.type === 'snippet'); });
+        q.statements = q.answer.annotations.filter(function (a) { return (a.type === 'statement'); });
+        delete q.answer.annotations;
+    }
+
+    if (q.question_type === 'decisive') {
+        q.question_type = 'yesno';
+    }
+
+    delete q.creator;
+    db.question.save(q);
+});
+
+db.question.find().forEach(function (q) {
+    q.statements.forEach(function (s) {
+        s.triples = [{ s: s.s, p: s.p, o: s.o }];
+        delete s.s;
+        delete s.p;
+        delete s.o;
+    });
+    db.question.save(q);
+});
